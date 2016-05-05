f52226601f78414c93d19f659270effd4ac32fa1
r-a-y <r-a-y@gmx.com>
2016-05-05 09:40:23 -0700

    Update v0.1.1 to use latest algorithm from v0.3.1.
    Newer versions of html2text uses namespaces, which is not compatible with
    < PHP 5.3.

--- html2text.php
+++ html2text.php
@@ -20,11 +20,14 @@
  *   <li>Information in the &lt;head&gt; is lost
  * </ul>
  *
- * @param string html the input HTML
- * @return string the HTML converted, as best as possible, to text
- * @throws Html2TextException if the HTML could not be loaded as a {@link DOMDocument}
+ * @param html the input HTML
+ * @return the HTML converted, as best as possible, to text
  */
 function convert_html_to_text($html) {
+	// replace &nbsp; with spaces
+	$html = str_replace("&nbsp;", " ", $html);
+	$html = str_replace("\xc2\xa0", " ", $html);
+
 	$html = fix_newlines($html);
 
 	$doc = new DOMDocument();
@@ -35,6 +38,10 @@ function convert_html_to_text($html) {
 
 	// remove leading and trailing spaces on each line
 	$output = preg_replace("/[ \t]*\n[ \t]*/im", "\n", $output);
+	$output = preg_replace("/ *\t */im", "\t", $output);
+
+	// remove unnecessary empty lines
+	$output = preg_replace("/\n\n\n*/im", "\n\n", $output);
 
 	// remove leading and trailing whitespace
 	$output = trim($output);
@@ -47,8 +54,8 @@ function convert_html_to_text($html) {
  * then \r becomes \n. This means that all newlines (Unix, Windows, Mac)
  * all become \ns.
  *
- * @param string text text with any number of \r, \r\n and \n combinations
- * @return string the fixed text
+ * @param text text with any number of \r, \r\n and \n combinations
+ * @return the fixed text
  */
 function fix_newlines($text) {
 	// replace \r\n to \n
@@ -75,6 +82,7 @@ function next_child_name($node) {
 
 	return $nextName;
 }
+
 function prev_child_name($node) {
 	// get the previous child
 	$nextNode = $node->previousSibling;
@@ -94,7 +102,7 @@ function prev_child_name($node) {
 
 function iterate_over_node($node) {
 	if ($node instanceof DOMText) {
-		return preg_replace("/[\\t\\n\\v\\f\\r ]+/im", " ", $node->wholeText);
+		return preg_replace("/[\\t\\n\\f\\r ]+/im", " ", $node->wholeText);
 	}
 	if ($node instanceof DOMDocumentType) {
 		// ignore
@@ -125,14 +133,27 @@ function iterate_over_node($node) {
 		case "h4":
 		case "h5":
 		case "h6":
-			// add two newlines
+		case "ol":
+		case "ul":
+			// add two newlines, second line is added below
 			$output = "\n";
 			break;
 
+		case "td":
+		case "th":
+			// add tab char to separate table fields
+		   $output = "\t";
+		   break;
+
+		case "tr":
 		case "p":
 		case "div":
 			// add one line
 			$output = "\n";
+			break;
+
+		case "li":
+			$output = "- ";
 			break;
 
 		default:
@@ -156,14 +177,6 @@ function iterate_over_node($node) {
 
 	// end whitespace
 	switch ($name) {
-		case "style":
-		case "head":
-		case "title":
-		case "meta":
-		case "script":
-			// ignore these tags
-			return "";
-
 		case "h1":
 		case "h2":
 		case "h3":
@@ -189,6 +202,24 @@ function iterate_over_node($node) {
 		case "a":
 			// links are returned in [text](link) format
 			$href = $node->getAttribute("href");
+
+			$output = trim($output);
+
+			// remove double [[ ]] s from linking images
+			if (substr($output, 0, 1) == "[" && substr($output, -1) == "]") {
+				$output = substr($output, 1, strlen($output) - 2);
+
+				// for linking images, the title of the <a> overrides the title of the <img>
+				if ($node->getAttribute("title")) {
+					$output = $node->getAttribute("title");
+				}
+			}
+
+			// if there is no link text, but a title attr
+			if (!$output && $node->getAttribute("title")) {
+				$output = $node->getAttribute("title");
+			}
+
 			if ($href == null) {
 				// it doesn't link anywhere
 				if ($node->getAttribute("name") != null) {
@@ -200,7 +231,12 @@ function iterate_over_node($node) {
 					$output;
 				} else {
 					// replace it
-					$output = "[$output]($href)";
+					if ($output) {
+						$output = "[$output]($href)";
+					} else {
+						// empty string
+						$output = $href;
+					}
 				}
 			}
 
@@ -210,6 +246,21 @@ function iterate_over_node($node) {
 					$output .= "\n";
 					break;
 			}
+			break;
+
+		case "img":
+			if ($node->getAttribute("title")) {
+				$output = "[" . $node->getAttribute("title") . "]";
+			} elseif ($node->getAttribute("alt")) {
+				$output = "[" . $node->getAttribute("alt") . "]";
+			} else {
+				$output = "";
+			}
+			break;
+
+		case "li":
+			$output .= "\n";
+			break;
 
 		default:
 			// do nothing
